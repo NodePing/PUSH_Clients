f9c4859335309a8cd02d658f75a6df0e6acf29bf
stratacast <courtney@towerDefense.localdomain>
2018-08-27 16:50:34 -0700

    Modified files to have proper output of 1/0, added an iocage jail check

--- POSIX/NodePingPUSHClient/modules/cassandra.sh
+++ POSIX/NodePingPUSHClient/modules/cassandra.sh
@@ -11,7 +11,11 @@ echo "$cassstatus" | while read -r line; do
     hasip=$(echo $line | awk '{ print $2 }' | egrep '\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b' | xargs)
     if [ -n "$hasip" ]; then
         echo "$sep"
-        echo $line | awk '{ printf "\"%s\":\"%s\"", $2, $1 }'
+        if [ $(echo $line | awk '{ printf $1 }') = "UN" ]; then
+            echo $line | awk '{ printf "\"%s\": 1", $2 }'
+        else
+            echo $line | awk '{ printf "\"%s\": 0", $2 }'
+        fi
         sep=","
     fi
 done
