833acd67961df1f66e370d29e7f96195502b5862
stratacast <none>
2018-09-13 11:30:58 -0700

    Removed executable from modules so the user can choose to make modules executable. Added POSIX scripts to check the status of a PostgreSQL and MySQL/MariraDB database

---
+++ POSIX/NodePingPUSHClient/modules/mysqlstat.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+CONFIG="./modules/.mysqlstat.conf"
+# Config file options shouldn't have spaces between the variable
+# and value, no quotes should surround the value
+
+# This module has a separate check file in case you need to supply
+# a password. If you do, be sure to set the permissions to something
+# that is secure like 0400
+
+if [ ! -f $CONFIG ]; then
+    touch $CONFIG
+    echo "username=" >> $CONFIG
+    echo "password=" >> $CONFIG
+    echo "host=localhost" >> $CONFIG
+    echo "querystring=SELECT COUNT(*) FROM information_schema.SCHEMATA;" >> $CONFIG
+
+    exit
+fi
+
+USER=$(grep "username" $CONFIG | cut -f 2- -d "=")
+PASSWORD=$(grep "password" $CONFIG | cut -f 2- -d "=")
+HOST=$(grep "host" $CONFIG | cut -f 2- -d "=")
+# QUERYSTRING="SELECT COUNT(*) FROM information_schema.SCHEMATA;"
+QUERYSTRING=$(grep "querystring" $CONFIG | cut -f 2- -d "=")
+
+if [ -z $PASSWORD ]; then
+    data=$(mysql -u"$USER" -h "$HOST"<<EOF
+$QUERYSTRING
+EOF
+)
+else
+    data=$(mysql -u"$USER" -h "$HOST" -p"$PASSWORD"<<EOF
+$QUERYSTRING
+EOF
+)
+fi
+
+echo $data | awk '{ print $2 }'
---
+++ POSIX/NodePingPUSHClient/modules/pgsqlstat.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+CONFIG="./modules/.pgsqlstat.conf"
+# Config file options shouldn't have spaces between the variable
+# and value, no quotes should surround the value
+
+# This module has a separate check file in case you need to supply
+# a password. If you do, be sure to set the permissions to something
+# that is secure like 0400
+
+if [ ! -f $CONFIG ]; then
+    touch $CONFIG
+    echo "username=postgres" >> $CONFIG
+    echo "password=" >> $CONFIG
+    echo "querystring=SELECT COUNT(*) datname from pg_database" >> $CONFIG
+
+    exit
+fi
+
+USER=$(grep "username" $CONFIG | cut -f 2- -d "=")
+PASSWORD=$(grep "password" $CONFIG | cut -f 2- -d "=")
+HOST=$(grep "host" $CONFIG | cut -f 2- -d "=")
+QUERYSTRING=$(grep "querystring" $CONFIG | cut -f 2- -d "=")
+# SELECT COUNT(*) datname from pg_database
+
+# result=$(sudo -u postgres psql -U postgres -h localhost -c "SELECT COUNT(*) datname from pg_database")
+result=$(su -c "psql -p \"$PASSWORD\" -c \"$QUERYSTRING\"" postgres)
+
+echo $result | awk '{ print $3 }'
