cab02d0375629c88b9e0986b7822f4f76cf12da7
stratacast <courthicks1@gmail.com>
2019-06-18 17:18:03 -0700

    PowerShell scripts for checking IP addresses on a host

---
+++ PowerShell/NodePingPowerShellPUSH/modules/ip_addrs/ip_addrs.ps1
@@ -0,0 +1,24 @@
+$acceptable_ips = Get-Content -Raw -Path modules\ip_addrs\ip_addrs.json | ConvertFrom-Json
+
+$ips = Get-NetIPAddress | Select-Object -Property IPAddress
+
+foreach ( $ip in $ips ) {
+    $addr = $ip.IPAddress
+
+    if ($acceptable_ips -contains $addr) {
+        continue
+    } elseif ($addr -eq "127.0.0.1") {
+        continue
+    } elseif ($addr -eq "::1") {
+        continue
+    } elseif ($addr.StartsWith("fe80")) {
+        continue
+    } elseif ($addr -match "'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'") {
+        continue
+    } elseif ($acceptable_ips -notcontains $addr) {
+        Write-Output "0"
+        Exit
+    }
+}
+
+Write-Output "1"
