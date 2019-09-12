78bbeb660a45c332b3586ecfaa99958c601ef274
stratacast <courthicks1@gmail.com>
2019-09-12 14:16:15 -0700

    Added some comments, renamed mongo_exe to mongo_path in mongo Powershell

--- POSIX/NodePingPUSHClient/modules/mongodbstat/variables.sh
+++ POSIX/NodePingPUSHClient/modules/mongodbstat/variables.sh
@@ -1,4 +1,9 @@
+# This string will be evaluated by mongo with the --eval flag
 eval_string="db.runCommand( {connectionStatus: 1});"
+# All of or a portion of what you expect in the result to be successful
 expected_message="\"ok\" : 1"
+# Optional user authentication
 username=""
+# Optional password authentication
 password=""
+
--- PowerShell/NodePingPowerShellPUSH/modules/mongodbstat/mongodbstat.ps1
+++ PowerShell/NodePingPowerShellPUSH/modules/mongodbstat/mongodbstat.ps1
@@ -2,14 +2,14 @@ $user_input = Get-Content -Raw -Path $PSScriptRoot\mongodbstat.json | ConvertFro
 
 $eval_string = $user_input.eval_string
 $expected_output = $user_input.expected_output
-$mongo_exe = $user_input.mongo_exe
+$mongo_path = $user_input.mongo_path
 $username = $user_input.username
 $password = $user_input.password
 
 if ( $username -eq "" -and $password -eq "" ) {
-    $result = & $mongo_exe --quiet --eval "$eval_string"
+    $result = & $mongo_path --quiet --eval "$eval_string"
 } else {
-    $result = & $mongo_exe --quiet --username "$username" --password "$password" --eval "$eval_string"
+    $result = & $mongo_path --quiet --username "$username" --password "$password" --eval "$eval_string"
 }
 
 if ( $result -match $expected_output ) {
