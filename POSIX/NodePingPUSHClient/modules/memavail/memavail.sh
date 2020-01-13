3d3d95c788386a8546ac894f019ad70ac35c509f
stratacast <courtney@towerDefense.localdomain>
2020-01-13 11:43:54 -0800

    Improved how OpenBSD gets free and cached memory

--- POSIX/NodePingPUSHClient/modules/memavail/memavail.sh
+++ POSIX/NodePingPUSHClient/modules/memavail/memavail.sh
@@ -6,9 +6,12 @@ if [ "$os" = "Linux" ]; then
     free -m | awk 'NR==2{printf  "%s", $7 }'
 elif [ "$os" = "FreeBSD" ]; then
     free=$(top -d1 | grep "Mem" | awk '{ for (i=1;i<=NF;i++) if ($i == "Free") print $(i-1) }' | sed 's/[^0-9]/ /g')
-    inact=$(top -d1 | grep "Mem" | awk '{for (i=1;i<NF;i++) if ($i == "Inact,") print$(i-1) }' | sed 's/[^0-9]/ /g')
+    inact=$(top -d1 | grep "Mem" | awk '{ for (i=1;i<NF;i++) if ($i == "Inact,") print $(i-1) }' | sed 's/[^0-9]/ /g')
 
     echo $(( "$free" + "$inact" ))
 elif [ "$os" = "OpenBSD" ]; then
-    top -d1 | grep Mem | awk '{ print $6,$8 }' | sed 's/[^0-9]/ /g' | awk '{ print $1 + $2 }'
+    free=$(top -d1 | grep "Memory" | awk '{ for (i=1;i<NF;i++) if ($i == "Free:") print $(i+1) }' | sed 's/[^0-9]/ /g')
+    cache=$(top -d1 | grep "Memory" | awk '{ for (i=1;i<NF;i++) if ($i == "Cache:") print $(i+1) }' | sed 's/[^0-9]/ /g')
+
+    echo $(( $free + $cache ))
 fi
