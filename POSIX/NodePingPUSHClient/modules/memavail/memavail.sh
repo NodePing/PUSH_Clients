e8ee265bf49d443bbdae045541953c1afce7d116
stratacast <courtney@towerDefense.localdomain>
2020-01-13 11:32:17 -0800

    Better handling of FreeBSD with different top outputs

--- POSIX/NodePingPUSHClient/modules/memavail/memavail.sh
+++ POSIX/NodePingPUSHClient/modules/memavail/memavail.sh
@@ -5,14 +5,10 @@ os=$(uname)
 if [ $os = "Linux" ]; then
     free -m | awk 'NR==2{printf  "%s", $7 }'
 elif [ $os = "FreeBSD" ]; then
-    # FreeBSD 12 changed output of top
-    is_11=$(uname -r | grep -o 11)
+    free=$(top -d1 | grep "Mem" | awk '{ for (i=1;i<=NF;i++) if ($i == "Free") print $(i-1) }' | sed 's/[^0-9]/ /g')
+    inact=$(top -d1 | grep "Mem" | awk '{for (i=1;i<NF;i++) if ($i == "Inact,") print$(i-1) }' | sed 's/[^0-9]/ /g')
 
-    if [ -z $is_11 ]; then
-        top -d1 | grep Mem | awk '{ print $4,$8,$10 }' | sed 's/[^0-9]/ /g' | awk '{ print $1 + $2 + $3 }'
-    else
-        top -d1 | grep Mem | awk '{ print $4,$8 }' | sed 's/[^0-9]/ /g' | awk '{ print $1 + $2 }'
-    fi
+    echo $(( "$free" + "$inact" ))
 elif [ $os = "OpenBSD" ]; then
     top -d1 | grep Mem | awk '{ print $6,$8 }' | sed 's/[^0-9]/ /g' | awk '{ print $1 + $2 }'
 fi
