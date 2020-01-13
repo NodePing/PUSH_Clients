41e9f8aaf6f31bcc1ea9d4cdc2437e6a6ad75c07
stratacast <courtney@towerDefense.localdomain>
2020-01-13 11:33:02 -0800

    Added quotes around os variable in if statements

--- POSIX/NodePingPUSHClient/modules/memavail/memavail.sh
+++ POSIX/NodePingPUSHClient/modules/memavail/memavail.sh
@@ -2,13 +2,13 @@
 
 os=$(uname)
 
-if [ $os = "Linux" ]; then
+if [ "$os" = "Linux" ]; then
     free -m | awk 'NR==2{printf  "%s", $7 }'
-elif [ $os = "FreeBSD" ]; then
+elif [ "$os" = "FreeBSD" ]; then
     free=$(top -d1 | grep "Mem" | awk '{ for (i=1;i<=NF;i++) if ($i == "Free") print $(i-1) }' | sed 's/[^0-9]/ /g')
     inact=$(top -d1 | grep "Mem" | awk '{for (i=1;i<NF;i++) if ($i == "Inact,") print$(i-1) }' | sed 's/[^0-9]/ /g')
 
     echo $(( "$free" + "$inact" ))
-elif [ $os = "OpenBSD" ]; then
+elif [ "$os" = "OpenBSD" ]; then
     top -d1 | grep Mem | awk '{ print $6,$8 }' | sed 's/[^0-9]/ /g' | awk '{ print $1 + $2 }'
 fi
