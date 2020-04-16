b1495f21814a02ac6bcb4a6d67c530022810c1d4
stratacast <courtney@towerDefense.localdomain>
2020-04-16 15:12:49 -0700

    Removed sudo and added full path to iptables and ip6tables

--- POSIX/NodePingPUSHClient/modules/checkiptables/checkiptables.sh
+++ POSIX/NodePingPUSHClient/modules/checkiptables/checkiptables.sh
@@ -3,7 +3,7 @@
 # Checks the count of iptables rules and alerts if the number of rules has changed.
 # User running this needs super user privileges
 
-ipv4rules=$(sudo iptables -n --list --line-numbers | sed '/^num\|^$\|^Chain/d' | wc -l)
-ipv6rules=$(sudo ip6tables -n --list --line-numbers | sed '/^num\|^$\|^Chain/d' | wc -l)
+ipv4rules=$(/sbin/iptables -n --list --line-numbers | sed '/^num\|^$\|^Chain/d' | wc -l)
+ipv6rules=$(/sbin/ip6tables -n --list --line-numbers | sed '/^num\|^$\|^Chain/d' | wc -l)
 
 printf "{\"ipv4\":%s\"ipv6\":%s}" $ipv4rules, $ipv6rules
